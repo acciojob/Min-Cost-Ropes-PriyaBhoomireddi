feb4bd8b27c8166d2798b5c2f4888de9ff298774
Priya Bhoomireddi <priyagetlucky1011@gmail.com>
2024-07-16 19:56:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+    let totalCost = 0;
+    
+    while (arr.length > 1) {
+        arr.sort((a, b) => a - b);
+       let first = arr.shift();
+        let second = arr.shift();
+        let cost = first + second;
+        totalCost += cost;
+        arr.push(cost);
+    }
+    
+    return totalCost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
